364d0931c9ad617f562fe41113cd34911ec3a66c
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-08-19 01:21:19 +0200

    Add ControlsBar tests

--- src/scenes/Library/components/ControlsBar/spec.jsx
+++ src/scenes/Library/components/ControlsBar/spec.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { expect } from "chai";
+import { fromJS } from "immutable";
 import ControlsBar from "./index";
 import PlayButton from "./PlayButton";
 import SkipButton from "./SkipButton";
 import DurationBar from "./DurationBar";
+import PlayState from "~/constants/PlayState";
+import ScrollingText from "~/components/ScrollingText";
 
 describe("ControlsBar", () => {
+    const nowPlaying = fromJS({
+        track: {
+            id: "a",
+            title: "Believa",
+            artist: "Raelee Nikole",
+            album: "Answers",
+            duration: 243,
+        },
+        currentTime: 203,
+        state: PlayState.PLAYING,
+    });
+
     it("renders itself", () => {
         const component = shallow(<ControlsBar />);
 
@@ -29,5 +44,18 @@ describe("ControlsBar", () => {
         const component = shallow(<ControlsBar />);
 
         expect(component).to.have.exactly(1).descendants(DurationBar);
+    });
+
+    it("displays the current title and artist", () => {
+        const component = shallow(<ControlsBar nowPlaying={nowPlaying} />);
+
+        expect(component).to.contain(<ScrollingText text="Believa" />);
+        expect(component).to.contain(<ScrollingText text="Raelee Nikole" />);
+    });
+
+    it("reflects the current play state", () => {
+        const component = shallow(<ControlsBar nowPlaying={nowPlaying} />);
+
+        expect(component.find(PlayButton)).to.have.prop("playing", true);
     });
 });

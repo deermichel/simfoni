10305349933c973bbd52b686c8fae3d37a7077c3
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-08-18 17:15:51 +0200

    Implement DurationBar

--- src/scenes/Library/components/ControlsBar/DurationBar/index.jsx
+++ src/scenes/Library/components/ControlsBar/DurationBar/index.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./style.scss";
 
-const DurationBar = () => (
-    <div className={styles.durationbar}>
+const propTypes = {
+    currentTime: PropTypes.number,
+    totalTime: PropTypes.number,
+};
+const defaultProps = {
+    currentTime: 0,
+    totalTime: 0,
+};
+
+const formatDuration = (duration) => `${Math.trunc(duration / 60)}:${(duration % 60).toString().padStart(2, "0")}`;
+const generateStyle = (currentTime, totalTime) => {
+    const progress = (totalTime === 0) ? 0 : 100.0 * currentTime / totalTime;
+    return {
+        background: `linear-gradient(to right, salmon ${progress}%, lightgrey ${progress}%)`,
+    };
+};
+
+const DurationBar = ({ currentTime, totalTime }) => (
+    <div
+        className={styles.durationbar}
+        style={generateStyle(currentTime, totalTime)}
+    >
         <div className={styles.details}>
             <span className={styles.currenttime}>
-                2:12
+                {formatDuration(currentTime)}
             </span>
             <span className={styles.totaltime}>
-                3:28
+                {formatDuration(totalTime)}
             </span>
         </div>
     </div>
 );
+
+DurationBar.propTypes = propTypes;
+DurationBar.defaultProps = defaultProps;
 
 export default DurationBar;
--- src/scenes/Library/components/ControlsBar/DurationBar/spec.jsx
+++ src/scenes/Library/components/ControlsBar/DurationBar/spec.jsx
@@ -9,4 +9,16 @@ describe("DurationBar", () => {
 
         expect(component).to.be.present();
     });
+
+    it("displays the current time", () => {
+        const component = shallow(<DurationBar currentTime={63} />);
+
+        expect(component).to.contain.text("1:03");
+    });
+
+    it("displays the total time", () => {
+        const component = shallow(<DurationBar totalTime={32} />);
+
+        expect(component).to.contain.text("0:32");
+    });
 });

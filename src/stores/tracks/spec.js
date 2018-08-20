293660e7ca0c62d7524a4f36c528226a5a101021
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-08-21 00:13:59 +0200

    Reorganize reducer test

--- src/stores/tracks/spec.js
+++ src/stores/tracks/spec.js
@@ -1,29 +1,27 @@
 import { expect } from "chai";
 import { fromJS, List } from "immutable";
+import actions from "./actions";
 import reducer from "./reducers";
 
 describe("tracks reducer", () => {
     it("handles SET_TRACKS", () => {
         const initialState = List();
-        const action = {
-            type: "SET_TRACKS",
-            tracks: [
-                {
-                    id: "a",
-                    title: "Believa",
-                    artist: "Raelee Nikole",
-                    album: "Answers",
-                    duration: 243,
-                },
-                {
-                    id: "b",
-                    title: "Pumped Up Kicks",
-                    artist: "Foster The People",
-                    album: "Torches",
-                    duration: 240,
-                },
-            ],
-        };
+        const action = actions.setTracks([
+            {
+                id: "a",
+                title: "Believa",
+                artist: "Raelee Nikole",
+                album: "Answers",
+                duration: 243,
+            },
+            {
+                id: "b",
+                title: "Pumped Up Kicks",
+                artist: "Foster The People",
+                album: "Torches",
+                duration: 240,
+            },
+        ]);
         const nextState = reducer(initialState, action);
 
         expect(nextState).to.equals(fromJS([
@@ -45,18 +43,15 @@ describe("tracks reducer", () => {
     });
 
     it("handles SET_TRACKS without initial state", () => {
-        const action = {
-            type: "SET_TRACKS",
-            tracks: [
-                {
-                    id: "a",
-                    title: "Believa",
-                    artist: "Raelee Nikole",
-                    album: "Answers",
-                    duration: 243,
-                },
-            ],
-        };
+        const action = actions.setTracks([
+            {
+                id: "a",
+                title: "Believa",
+                artist: "Raelee Nikole",
+                album: "Answers",
+                duration: 243,
+            },
+        ]);
         const nextState = reducer(undefined, action);
 
         expect(nextState).to.equals(fromJS([

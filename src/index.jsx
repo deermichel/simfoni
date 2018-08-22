57ff6a798b33e604f18d4016d9cae9a7c66bdc50
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-08-23 01:01:44 +0200

    Add further demo content

--- src/index.jsx
+++ src/index.jsx
@@ -9,27 +9,45 @@ import { tracksOperations } from "./stores/tracks";
 const store = configureStore();
 store.dispatch(tracksOperations.setTracks([
     {
-        id: "a",
+        id: "believa",
         title: "Believa",
         artist: "Raelee Nikole",
         album: "Answers",
         duration: 243, // 4:03
     },
     {
-        id: "b",
+        id: "pumped",
         title: "Pumped Up Kicks",
         artist: "Foster The People",
         album: "Torches",
         duration: 240, // 4:00
     },
     {
-        id: "c",
+        id: "no",
         title: "No Diggity",
         artist: "Chet Faker",
         album: "Digging the Blogosphere",
         duration: 226, // 3:46
     },
+    {
+        id: "born",
+        title: "Born Without Borders (Unplugged)",
+        artist: "The New Schematics",
+        album: "The New Schematics (Unplugged+)",
+        duration: 263, // 4:23
+    },
+    {
+        id: "it",
+        title: "It Was You",
+        artist: "Norah Jones",
+        album: "It Was You",
+        duration: 331, // 5:31
+    },
 ]));
+
+// store.subscribe(() => {
+//     console.log(store.getState().nowPlaying.toJS());
+// });
 
 const app = (
     <Provider store={store}>

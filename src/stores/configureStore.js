c612ea2fe3de988831875d36868e72bea5878d4c
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-08-22 23:18:27 +0200

    Improve logger

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,8 +1,21 @@
 import { createStore, applyMiddleware } from "redux";
+import { Iterable } from "immutable";
 import { createLogger } from "redux-logger";
 import rootReducer from "./rootReducer";
 
-const loggerMiddleware = createLogger();
+const stateTransformer = (state) => {
+    const newState = {};
+    Object.keys(state).forEach((i) => {
+        if (Iterable.isIterable(state[i])) {
+            newState[i] = state[i].toJS();
+        } else {
+            newState[i] = state[i];
+        }
+    });
+    return newState;
+};
+
+const loggerMiddleware = createLogger({ stateTransformer });
 
 const configureStore = (initialState) => createStore(
     rootReducer,

78868e362a09b3f13a48d0cb52344dcc879be4d3
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-08-29 20:53:28 +0200

    Create mock menu

--- src/renderer/components/App/components/Menu/index.jsx
+++ src/renderer/components/App/components/Menu/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+    Disc,
+    List,
+    Settings,
+    User,
+} from "react-feather";
 import styles from "./style.scss";
+import MenuItem from "./components/MenuItem";
 
 const propTypes = {
     show: PropTypes.bool,
@@ -13,7 +20,31 @@ const defaultProps = {
 
 const Menu = ({ show }) => (
     <div className={styles.menu}>
-        <div className={(show) ? styles.container : styles.hidden} />
+        <div className={(show) ? styles.container : styles.hidden}>
+
+            <div className={styles.section}>
+                <span className={styles.divider}>
+                    Views
+                </span>
+                <MenuItem text="Artists" icon={<User size={18} />} />
+                <MenuItem text="Albums" icon={<Disc size={18} />} />
+                <MenuItem text="Songs" icon={<List size={18} />} />
+                <MenuItem text="Settings" icon={<Settings size={18} />} />
+            </div>
+
+            <div className={styles.section}>
+                <span className={styles.divider}>
+                    Playlists
+                </span>
+                <MenuItem text="Me, Myself & I" />
+                <MenuItem text="New Songs" />
+                <MenuItem text="Often Played" />
+                <MenuItem text="Christian Mix" />
+                <MenuItem text="Christmas" />
+                <MenuItem text="Roadtrip" />
+            </div>
+
+        </div>
     </div>
 );
 
--- src/renderer/components/App/components/Menu/spec.jsx
+++ src/renderer/components/App/components/Menu/spec.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import { shallow } from "enzyme";
 import { expect } from "chai";
 import Menu from "./index";
+import MenuItem from "./components/MenuItem";
 
 describe("Menu", () => {
     it("renders itself", () => {
         const component = shallow(<Menu />);
 
         expect(component).to.be.present();
+        expect(component).to.contain.text("Views");
+        // expect(component).to.contain.text("Playlists");
+    });
+
+    it("has a MenuItem for Songs", () => {
+        const component = shallow(<Menu />);
+
+        expect(component.find({ text: "Songs" }).type()).to.equal(MenuItem);
+    });
+
+    it("has a MenuItem for Settings", () => {
+        const component = shallow(<Menu />);
+
+        expect(component.find({ text: "Settings" }).type()).to.equal(MenuItem);
     });
 });

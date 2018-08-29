26021387ddb6da33f0465a057d5ed7dcbbf96d6f
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-08-29 22:27:08 +0200

    Add Button to MenuItem

--- src/renderer/components/App/components/Menu/components/MenuItem/index.jsx
+++ src/renderer/components/App/components/Menu/components/MenuItem/index.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./style.scss";
+import Button from "~/components/Button";
 
 const propTypes = {
     text: PropTypes.string,
     active: PropTypes.bool,
     icon: PropTypes.node,
+    onClick: PropTypes.func,
 };
 const defaultProps = {
     text: "",
     active: false,
     icon: null,
+    onClick: () => 0,
 };
 
-const MenuItem = ({ text, active, icon }) => (
-    <div className={(active) ? styles.active : styles.inactive}>
-        {icon && (
-            <div className={styles.icon}>
-                {icon}
+const MenuItem = ({
+    text,
+    active,
+    icon,
+    onClick,
+}) => (
+    <div className={styles.menuitem}>
+        <Button onClick={onClick}>
+            <div className={(active) ? styles.active : styles.inactive}>
+                {icon && (
+                    <div className={styles.icon}>
+                        {icon}
+                    </div>
+                )}
+                {text}
             </div>
-        )}
-        {text}
+        </Button>
     </div>
 );
 
--- src/renderer/components/App/components/Menu/components/MenuItem/spec.jsx
+++ src/renderer/components/App/components/Menu/components/MenuItem/spec.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import { expect } from "chai";
 import { List } from "react-feather";
 import MenuItem from "./index";
+import Button from "~/components/Button";
 
 describe("MenuItem", () => {
     it("renders itself", () => {
@@ -12,7 +13,7 @@ describe("MenuItem", () => {
     });
 
     it("displays its text", () => {
-        const component = shallow(<MenuItem text="Hello Item" />);
+        const component = mount(<MenuItem text="Hello Item" />);
 
         expect(component).to.contain.text("Hello Item");
     });
@@ -21,5 +22,14 @@ describe("MenuItem", () => {
         const component = shallow(<MenuItem icon={<List />} />);
 
         expect(component).to.have.exactly(1).descendants(List);
+    });
+
+    it("invokes callback on click", () => {
+        let callbackInvoked = false;
+        const onClick = () => { callbackInvoked = true; };
+        const component = shallow(<MenuItem onClick={onClick} />);
+        component.find(Button).simulate("click");
+
+        expect(callbackInvoked).to.equal(true);
     });
 });

7e28c5ad6c2cddfec7cd43edf66277f9f7ad6b6c
Micha Hanselmann <micha.hanselmann@gmail.com>
2018-09-01 22:12:05 +0200

    Add tests for SearchPaths

--- src/renderer/views/Settings/components/SearchPaths/spec.jsx
+++ src/renderer/views/Settings/components/SearchPaths/spec.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import { expect } from "chai";
+import { fromJS } from "immutable";
+import { Delete, Plus } from "react-feather";
+import Clickable from "~/components/Clickable";
+import IconButton from "~/components/IconButton";
 import SearchPaths from "./index";
 
 describe("SearchPaths", () => {
@@ -8,5 +12,35 @@ describe("SearchPaths", () => {
         const component = shallow(<SearchPaths />);
 
         expect(component).to.be.present();
+    });
+
+    it("displays the paths", () => {
+        const paths = fromJS(["pathA", "pathB"]);
+        const component = shallow(<SearchPaths paths={paths} />);
+
+        expect(component).to.contain.text("pathA");
+        expect(component).to.contain.text("pathB");
+    });
+
+    it("has a delete button for each path", () => {
+        const removed = [];
+        const paths = fromJS(["pathA", "pathB"]);
+        const onRemove = (path) => removed.push(path);
+        const component = mount(<SearchPaths paths={paths} onRemove={onRemove} />);
+
+        expect(component.find({ icon: Delete })).to.have.lengthOf(2);
+
+        component.find(IconButton).at(1).find(Clickable).simulate("click");
+        expect(removed).to.include("pathA");
+
+        component.find(IconButton).at(2).find(Clickable).simulate("click");
+        expect(removed).to.include("pathB");
+    });
+
+    it("has one add button", () => {
+        const component = shallow(<SearchPaths />);
+
+        expect(component.find({ icon: Plus })).to.have.lengthOf(1);
+        expect(component.find({ icon: Plus }).type()).to.equal(IconButton);
     });
 });

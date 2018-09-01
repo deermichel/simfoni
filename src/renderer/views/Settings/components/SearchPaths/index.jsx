99b1c360d46293cb430056a84f3d87d3a8f02313
Micha Hanselmann <micha.hanselmann@gmail.com>
2018-09-01 20:46:37 +0200

    Update SearchPaths component

--- src/renderer/views/Settings/components/SearchPaths/index.jsx
+++ src/renderer/views/Settings/components/SearchPaths/index.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import ImmutablePropTypes from "react-immutable-proptypes";
 import { List } from "immutable";
 import { Delete, Plus } from "react-feather";
-import IconButton from "~/components/IconButton";
+import IconButton, { ButtonSize } from "~/components/IconButton";
 import styles from "./style.scss";
 
 const propTypes = {
     paths: ImmutablePropTypes.list,
+    onAdd: PropTypes.func,
+    onDelete: PropTypes.func,
 };
 const defaultProps = {
     paths: List(),
+    onAdd: () => 0,
+    onDelete: () => 0,
 };
 
-const SearchPaths = ({ paths }) => (
+const SearchPaths = ({ paths, onAdd, onDelete }) => (
     <div className={styles.searchpaths}>
         <div className={styles.title}>
             Search Paths
@@ -21,12 +26,18 @@ const SearchPaths = ({ paths }) => (
         <div className={styles.container}>
             <div className={styles.header}>
                 Search songs in:
-                <IconButton icon={<Plus size={20} />} />
+                <IconButton icon={Plus} size={ButtonSize.SMALL} onClick={onAdd} />
             </div>
+
             {paths.map((path) => (
                 <div className={styles.pathitem}>
                     {path}
-                    <IconButton icon={<Delete size={20} />} destructive />
+                    <IconButton
+                        icon={Delete}
+                        size={ButtonSize.SMALL}
+                        destructive
+                        onClick={() => onDelete(path)}
+                    />
                 </div>
             ))}
         </div>

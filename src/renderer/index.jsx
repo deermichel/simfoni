d0b27da4f880cae24ec3c747fc990fe9d2677fce
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-09-24 19:58:44 +0200

    Migrate to redux-persist

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -3,72 +3,34 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createMemoryHistory } from "history";
 import { ConnectedRouter } from "connected-react-router";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import { Switch, Route, Redirect } from "react-router";
 import App from "./components/App";
 import Songs from "./views/Songs";
 import Settings from "./views/Settings";
 import configureStore from "./stores/configureStore";
-import { settingsOperations } from "./stores/settings";
-import { libraryOperations } from "./stores/library";
 import Views from "./constants/Views";
 
 const history = createMemoryHistory();
 const store = configureStore(history);
-
-// load settings
-store.dispatch(settingsOperations.loadSearchPaths());
-
-[
-    {
-        title: "Believa",
-        artist: "Raelee Nikole",
-        album: "Answers",
-        duration: 243, // 4:03
-        uri: "_mp3/Believa.mp3",
-    },
-    {
-        title: "Pumped Up Kicks",
-        artist: "Foster The People",
-        album: "Torches",
-        duration: 240, // 4:00
-        uri: "_mp3/Pumped Up Kicks.mp3",
-    },
-    {
-        title: "No Diggity",
-        artist: "Chet Faker",
-        album: "Digging the Blogosphere",
-        duration: 226, // 3:46
-        uri: "_mp3/No Diggity.mp3",
-    },
-    {
-        title: "Born Without Borders (Unplugged)",
-        artist: "The New Schematics",
-        album: "The New Schematics (Unplugged+)",
-        duration: 263, // 4:23
-        uri: "_mp3/Born Without Borders (Unplugged).mp3",
-    },
-    {
-        title: "It Was You",
-        artist: "Norah Jones",
-        album: "It Was You",
-        duration: 331, // 5:31
-        uri: "_mp3/It Was You.mp3",
-    },
-].forEach((track) => store.dispatch(libraryOperations.addTrack(track)));
+const persistor = persistStore(store);
 
 const app = (
     <Provider store={store}>
-        <App>
-            <ConnectedRouter history={history}>
-                <Switch>
-                    <Route path={Views.ARTISTS} />
-                    <Route path={Views.ALBUMS} />
-                    <Route path={Views.SONGS} component={Songs} />
-                    <Route path={Views.SETTINGS} component={Settings} />
-                    <Redirect to={Views.SONGS} />
-                </Switch>
-            </ConnectedRouter>
-        </App>
+        <PersistGate loading="Loading..." persistor={persistor}>
+            <App>
+                <ConnectedRouter history={history}>
+                    <Switch>
+                        <Route path={Views.ARTISTS} />
+                        <Route path={Views.ALBUMS} />
+                        <Route path={Views.SONGS} component={Songs} />
+                        <Route path={Views.SETTINGS} component={Settings} />
+                        <Redirect to={Views.SONGS} />
+                    </Switch>
+                </ConnectedRouter>
+            </App>
+        </PersistGate>
     </Provider>
 );
 
--- src/renderer/stores/settings/operations.js
+++ src/renderer/stores/settings/operations.js
@@ -1,43 +1,8 @@
-import storage from "electron-json-storage";
 import actions from "./actions";
 
-const addSearchPaths = (paths) => (dispatch, getState) => {
-    dispatch(actions.addSearchPaths(paths));
-
-    const searchPaths = getState().settings.get("searchPaths");
-    storage.set("settings.searchpaths", searchPaths.toJSON(), (error) => {
-        if (error) {
-            // TODO: error handling
-        }
-    });
-};
-
-const removeSearchPath = (path) => (dispatch, getState) => {
-    dispatch(actions.removeSearchPath(path));
-
-    const searchPaths = getState().settings.get("searchPaths");
-    storage.set("settings.searchpaths", searchPaths.toJSON(), (error) => {
-        if (error) {
-            // TODO: error handling
-        }
-    });
-};
-
-const loadSearchPaths = () => (dispatch) => {
-    storage.get("settings.searchpaths", (error, paths) => {
-        if (error) {
-            // TODO: error handling
-            return;
-        }
-
-        if (Object.keys(paths).length > 0) {
-            dispatch(actions.addSearchPaths(paths));
-        }
-    });
-};
+const { addSearchPaths, removeSearchPath } = actions;
 
 export default {
     addSearchPaths,
     removeSearchPath,
-    loadSearchPaths,
 };
